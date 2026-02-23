e3af5565b52d8669d237985fb4978117fd34e710
sisshmue <sisshmue404@gmail.com>
2026-02-23 11:48:07 +0700

    make the mobile responsive ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,36 @@
-import './Home.css'
+import "./Home.css";
 
 export function Home() {
-    return (
-        <div className='home-section-container'> 
-            <h1 className="hero-text">{`SOFTWARE\nENGINEER`}</h1>
-            <p className="sub-text">Building sleek, high-performance web experiences.</p>
-            
-            <div className='stats-grid'>
-                <div className='stat-card'>
-                    <h2 className='stat-value'>+3</h2>
-                    <p className='stat-label'>{`YEARS OF\nExperience`}</p>
-                </div>
-                <div className='stat-card'>
-                    <h2 className='stat-value'>3.92</h2>
-                    <p className='stat-label'>{`UNIVERSITY\nGPA`}</p>
-                </div>
-                <div className='stat-card'>
-                    <h2 className='stat-value'>+40</h2>
-                    <p className='stat-label'>{`PERSONAL\nPROJECTS`}</p>
-                </div>
-            </div>
+  return (
+    <div className="home-section-container">
+      <h1 className="hero-text">{`SOFTWARE\nENGINEER`}</h1>
+      <p className="sub-text">
+        🎓 A Dedicated software engineering student with a focus on
+        cross-platform and full-stack for both mobile and web application
+        development. I am passionate about crafting intuitive and accessible
+        user experiences across diverse devices. Through practical experience
+        with frameworks such as Flutter and MERN Stack, I have honed my skills
+        in implementing seamless navigation, advanced API integrations, and
+        robust application architecture. I thrive in collaborative environments
+        and am committed to continuous learning in this rapidly evolving field.
+        My aim is to contribute to innovative projects that positively impact
+        users worldwide.
+      </p>
+
+      <div className="stats-grid">
+        <div className="stat-card">
+          <h2 className="stat-value">+3</h2>
+          <p className="stat-label">{`YEARS OF\nExperience`}</p>
         </div>
-    )
+        <div className="stat-card">
+          <h2 className="stat-value">3.92</h2>
+          <p className="stat-label">{`UNIVERSITY\nGPA`}</p>
+        </div>
+        <div className="stat-card">
+          <h2 className="stat-value">+40</h2>
+          <p className="stat-label">{`PERSONAL\nPROJECTS`}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
